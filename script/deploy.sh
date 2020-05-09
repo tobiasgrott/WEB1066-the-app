89a908c23c4c6be84c4aff7bdcbe177f9b48db5f
Tobias Grott <tobiasgrott@gmail.com>
2020-05-09 16:17:30 +0200

    removed docker pull from deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-#set -e
+set -e
 
 echo "Logging in to Docker"
 echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin 
-echo "Pulling Dockerfile"
-docker pull zutherb/monolithic-shop
 echo "Tagging Dockerfile"
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 echo "Pushing to Registry"
