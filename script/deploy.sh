0c8b5457f916d842c2fab1fb61da4677830c3f5c
Tobias Grott <tobiasgrott@gmail.com>
2020-05-09 15:04:24 +0200

    updated deploy condition

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,5 @@
 set -e
 
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin ;"
-docker pull zutherb/monolithic-shop:latest
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
