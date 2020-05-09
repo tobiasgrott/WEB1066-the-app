d9b51376d1661f11746bc187fcdb0b212126c77a
Tobias Grott <tobiasgrott@gmail.com>
2020-05-09 16:38:23 +0200

    Revert "Question 10"

--- script/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Starting deploy"
-echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
-docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
-docker push $DOCKER_LOGIN/monolithic-shop:latest
