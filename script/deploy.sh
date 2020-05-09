b1901599060cc72cecfa60ebd2fdc8715f513d03
Tobias Grott <tobiasgrott@gmail.com>
2020-05-09 16:22:36 +0200

    updated deployment script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Logging in to Docker"
-echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin 
-echo "Tagging Dockerfile"
+echo "Starting deploy"
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
-echo "Pushing to Registry"
 docker push $DOCKER_LOGIN/monolithic-shop:latest
-echo "Deployment Done"
-
-
