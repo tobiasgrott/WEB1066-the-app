d60683b6031aba92ef5b7085e507a9dcf88af4b6
Tobias Grott <tobiasgrott@gmail.com>
2020-05-09 13:55:39 +0200

    first try question 10

---
+++ script/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin ;"
+docker pull zutherb/monolithic-shop:latest
+docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
+docker push $DOCKER_LOGIN/monolithic-shop:latest
